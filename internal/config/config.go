8ccb25ed1e5802609f29c4b1167e97dd3c84e317
cczywyc <274230928@qq.com>
2024-04-21 18:45:40 +0800

    config

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,5 +1,13 @@
 package config
 
+import (
+	"flag"
+	"fmt"
+	"gopkg.in/yaml.v3"
+	"os"
+	"strings"
+)
+
 var Cfg *Config
 
 type Config struct {
@@ -9,8 +17,7 @@ type Config struct {
 }
 
 type Log struct {
-	Level string `yaml:"level"`
-	Path  string `yaml:"path"`
+	Path string `yaml:"path"`
 }
 
 type Database struct {
@@ -18,4 +25,48 @@ type Database struct {
 	User   string `yaml:"user"`
 	Passwd string `yaml:"passwd"`
 	DbName string `yaml:"dbname"`
+}
+
+// init the config into global config point from the config file
+func init() {
+	cfgFile := flag.String("config_file", "", "config file path")
+	flag.Parse()
+
+	if "" != *cfgFile || len(strings.TrimSpace(*cfgFile)) != 0 {
+		cfgContent, err := os.ReadFile(*cfgFile)
+		if err != nil {
+			err := fmt.Errorf("read config file error: %s", err.Error())
+			panic(err)
+		}
+		Cfg = &Config{}
+		err = yaml.Unmarshal(cfgContent, Cfg)
+		if err != nil {
+			err := fmt.Errorf("unmarshal config file error: %s", err.Error())
+			panic(err)
+		}
+	}
+}
+
+// init the config into global config point from the env
+func init() {
+	port, portExist := os.LookupEnv("HTTP_PORT")
+	host, hostExist := os.LookupEnv("DB_HOST")
+	user, userExist := os.LookupEnv("DB_USER")
+	passwd, passwdExist := os.LookupEnv("DB_PASSWORD")
+	dbName, dbNameExist := os.LookupEnv("DB_NAME")
+	if portExist {
+		Cfg.Port = port
+	}
+	if hostExist {
+		Cfg.Database.Host = host
+	}
+	if userExist {
+		Cfg.Database.User = user
+	}
+	if passwdExist {
+		Cfg.Database.Passwd = passwd
+	}
+	if dbNameExist {
+		Cfg.Database.DbName = dbName
+	}
 }
