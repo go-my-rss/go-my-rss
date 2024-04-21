a12fd0ea8de2cfc3148c46094e6c6ade5c59197f
cczywyc <274230928@qq.com>
2024-04-21 14:56:10 +0800

    config struct

---
+++ internal/config/config.go
@@ -0,0 +1,19 @@
+package config
+
+type Config struct {
+	Port     string   `yaml:"port"`
+	Log      Log      `yaml:"log"`
+	Database Database `yaml:"database"`
+}
+
+type Log struct {
+	Level string `yaml:"level"`
+	Path  string `yaml:"path"`
+}
+
+type Database struct {
+	Host   string `yaml:"host"`
+	User   string `yaml:"user"`
+	Passwd string `yaml:"passwd"`
+	DbName string `yaml:"dbname"`
+}
